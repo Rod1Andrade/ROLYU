25d72141fcc88f04f06dc95d3a7d5f782bdd1a80
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2020-10-18 16:46:55 -0300

    UPDATE - Entry Point

--- programm/src/App/Application.java
+++ programm/src/App/Application.java
@@ -1,12 +1,36 @@
 package App;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Classe Principal
+ * Classe Principal - Entry point
  */
-public class Application {
+public class Application extends JFrame {
+
+    /**
+     * Constructor
+     * @param title
+     */
+    private Application(String title) {
+        this.setTitle(title);
+        this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /**
+     * Roda aplicacao com o componente
+     * que deve ser iniciado juntamente.
+     * @param component Componente
+     */
+    private void run(JComponent component) {
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.add(component);
+    }
 
     public static void main(String[] args) {
-
+        new Application("ROLYU").run(new JPanel());
     }
 
 }
